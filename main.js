270da8ac20e5dc1e3e6800825086df7d3b9f39ae
ronentgreat <ronenh53@gmail.com>
2022-04-17 22:07:37 +0000

    completed basic form validation

---
+++ main.js
@@ -0,0 +1,26 @@
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let form = id("form"),
+    input = id("input"),
+    msg = id("msg")
+    posts = id("posts")
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("button clicked")
+
+  formValidation();
+});
+
+let formValidation = () => {
+  if(input.value.trim() === ""){
+    msg.innerText = "Post cannot be black";
+    console.log("failure");
+  }else{
+    console.log("success");
+    msg.innerHTML = "";
+  }
+};
